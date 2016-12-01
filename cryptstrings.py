dbcc9a65c0936a233d0fcd808cda56ed4dcaade0
Uros Orozel <uros.orozel@gmail.com>
2016-12-01 15:44:38 +1100

    Added support for files and multiple keys

--- cryptstrings.py
+++ cryptstrings.py
@@ -5,8 +5,10 @@
 Usage:
   cryptstrings listkeys
   cryptstrings createkeys [--key-name=<key_name_suffix>] [--key-size=1024] [--keys-path=<keys_path>]
-  cryptstrings encrypt <input_file> <keyname> <output_file> [--public-key=<public-key.pem>]
-  cryptstrings decrypt <input_file> <keyname> <output_file> [--private-key=<private-key.pem>]
+  cryptstrings string encrypt <input_yaml> <keyname>... [--public-key=<public-key.pem>]
+  cryptstrings string decrypt <input_yaml> <keyname>... [--private-key=<private-key.pem>]
+  cryptstrings file encrypt <input_file>... [--public-key=<public-key.pem>]
+  cryptstrings file decrypt <input_file>... [--private-key=<private-key.pem>]
   cryptstrings -h | --help
   cryptstrings --version
 
@@ -19,11 +21,15 @@ Options:
 http://nvie.com/posts/modifying-deeply-nested-structures/
 https://gist.github.com/nvie/f304caf3b4f1ca4c3884
 https://docs.launchkey.com/developer/encryption/python/python-encryption.html
+http://www.blog.pythonlibrary.org/2016/05/18/python-3-an-intro-to-encryption/
+https://github.com/massenz/filecrypt
 """
 
 import os
 import sys
 import os.path
+from os.path import expanduser
+from sh import ErrorReturnCode, openssl
 import json
 import yaml
 import glob
@@ -33,15 +39,18 @@ from distutils.util import strtobool
 from docopt import docopt
 from Crypto import Random
 from Crypto.PublicKey import RSA
-from Crypto.Cipher import PKCS1_OAEP
+from Crypto.Cipher import AES,PKCS1_OAEP
 
+# PyCryptodome
 # Default values
 RSA_KEY_NAME = "cryptstrings"
 RSA_PRIVATE_KEY = "_private.pem"
 RSA_PUBLIC_KEY = "_public.pem"
-RSA_KEYS_PATH = ".cryptstrings"
+RSA_KEYS = ".cryptstrings"
+RSA_KEYS_PATH = os.path.join(expanduser('~'), RSA_KEYS)
 RSA_MAGIC = "RSA:"
 RSA_KEY_SIZE = 1024
+DECRYPT_SUFF="_decrypted"
 
 PRIVATE_KEY = os.path.join(RSA_KEYS_PATH, RSA_KEY_NAME + RSA_PRIVATE_KEY)
 PUBLIC_KEY = os.path.join(RSA_KEYS_PATH, RSA_KEY_NAME + RSA_PUBLIC_KEY)
@@ -103,6 +112,11 @@ def write_yaml(filename, json):
         yaml.dump(json, outfile, default_flow_style=False)
 
 
+def write_file(filename, data):
+    with open(filename, 'w') as outfile:
+        outfile.write(data)
+
+
 def create_keys(key_size=RSA_KEY_SIZE, key_name=RSA_KEY_NAME, keys_path=RSA_KEYS_PATH):
     random_generator = Random.new().read
     key = RSA.generate(key_size, random_generator)
@@ -132,6 +146,16 @@ def load_yaml(yaml_path):
         sys.exit(1)
 
 
+def load_file(file_path):
+    try:
+        with open(file_path, 'r') as file_data:
+            load = file_data.read()
+        return load
+    except IOError as e:
+        print "I/O error({0}): {1} {2}".format(e.errno, e.strerror, file_path)
+        sys.exit(1)
+
+
 def process_string(attribute):
     if encrypt:
         if not attribute.startswith(RSA_MAGIC):
@@ -146,6 +170,61 @@ def process_string(attribute):
             attribute = decrypt_RSA(attribute)
             return attribute
 
+def encrypt_file(filename):
+    public_key = PUBLIC_KEY
+    enc_file = filename[:-len(DECRYPT_SUFF)]
+    print enc_file
+    with open(filename, 'rb') as plain_file:
+
+        openssl('rsautl', '-encrypt', '-inkey', PUBLIC_KEY, '-pubin', _in=plain_file, _out=enc_file)
+    return True
+
+def decrypt_file(filename):
+    private_key = PRIVATE_KEY
+    plain_file = filename + DECRYPT_SUFF
+    with open(filename, 'rb') as enc_file:
+        openssl('enc', '-aes-256-cbc','-d', '-pass', ('file:%s' % PRIVATE_KEY), _in=enc_file,_out=plain_file)
+    return True
+
+def encrypt_RSA_file(message):
+    public_key = PUBLIC_KEY
+    key = open(public_key, "r").read()
+    rsakey = RSA.importKey(key)
+    rsakey = PKCS1_OAEP.new(rsakey)
+    encrypted = rsakey.encrypt(message)
+    return encrypted.encode('base64')
+
+def encrypt_RSA_files(filename, data):
+    public_key = PUBLIC_KEY
+    print public_key
+    with open(filename, 'w') as output_file:
+        key = open(public_key, "r").read()
+        session_key = Random.get_random_bytes(16)
+        key = RSA.importKey(key)
+        cipher_rsa = PKCS1_OAEP.new(key)
+        output_file.write(cipher_rsa.encrypt(session_key))
+        cipher_aes = AES.new(session_key, AES.MODE_EAX)
+        ciphertext,tag = cipher_aes.encrypt_and_digest(data)
+
+        output_file.write(cipher_aes.nonce)
+        output_file.write(tag)
+        output_file.write(ciphertext)
+
+def decrypt_RSA_files(filename):
+    private_key = PRIVATE_KEY
+    print private_key
+    with open(filename, 'rb') as fobj:
+        private_key = RSA.import_key(open(private_key).read())
+        enc_session_key, nonce, tag, ciphertext = [ fobj.read(x) 
+                                            for x in (private_key.size_in_bytes(), 
+                                            16, 16, -1) ]
+ 
+        cipher_rsa = PKCS1_OAEP.new(private_key)
+        session_key = cipher_rsa.decrypt(enc_session_key)
+ 
+        cipher_aes = AES.new(session_key, AES.MODE_EAX, nonce)
+        data = cipher_aes.decrypt_and_verify(ciphertext, tag)
+    return data
 
 def encrypt_RSA(message):
     public_key = PUBLIC_KEY
@@ -175,7 +254,7 @@ def traverse_and_modify(obj, attribute, callback, key=None):
 
     if value is None:
         return value
-    
+
     reg = re.search(attribute, key)
 
     if reg:
@@ -186,15 +265,20 @@ def traverse_and_modify(obj, attribute, callback, key=None):
 if __name__ == '__main__':
     arguments = docopt(__doc__, version='1.0.1rc')
     # sub command
+    print(arguments)
     createkeys = arguments['createkeys']
     listkeys = arguments['listkeys']
+    file = arguments['file']
+    string = arguments['string']
     encrypt = arguments['encrypt']
     decrypt = arguments['decrypt']
 
     if encrypt or decrypt:
         input_file = arguments['<input_file>']
+        input_yaml = arguments['<input_yaml>']
         keyname = arguments['<keyname>']
-        output_file = arguments['<output_file>']
+        #output_file = arguments['<output_file>']
+
 
     if createkeys:
         # options
@@ -213,26 +297,56 @@ if __name__ == '__main__':
         else:
             keys_path = RSA_KEYS_PATH
 
-        file_path = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)), keys_path)
+        # os.path.abspath(__file__)
+        file_path = os.path.join(expanduser('~'), keys_path)
         ensure_dir(file_path)
         create_keys(key_size, key_name, keys_path)
 
-    if encrypt:
-        if arguments['--public-key']:
-            PUBLIC_KEY = arguments['--public-key']
-        check_file(PUBLIC_KEY)
-        json_data = load_yaml(input_file)
-        modified = traverse_and_modify(json_data, keyname, process_string)
-        write_yaml(output_file, modified)
+    if string:
+        if encrypt:
+            if not input_yaml.endswith(DECRYPT_SUFF):
+                print("!!! WARNING !!!\nAll input files have to have a suffix: '%s' to be able filter them with gitignore etc.\n!!! WARNING !!!" % DECRYPT_SUFF)
+                sys.exit(1)
+            if arguments['--public-key']:
+                PUBLIC_KEY = arguments['--public-key']
+            check_file(PUBLIC_KEY)
+            json_data = load_yaml(input_yaml)
+            for key in keyname:
+                json_data = traverse_and_modify(json_data, key, process_string)
+            write_yaml(input_yaml[:-len(DECRYPT_SUFF)], json_data)
 
-    if decrypt:
-        if arguments['--private-key']:
-            PRIVATE_KEY = arguments['--private-key']
-        check_file(PRIVATE_KEY)
-        json_data = load_yaml(input_file)
-        modified = traverse_and_modify(json_data, keyname, process_string)
-        write_yaml(output_file, modified)
+        if decrypt:
+            if arguments['--private-key']:
+                PRIVATE_KEY = arguments['--private-key']
+            check_file(PRIVATE_KEY)
+            json_data = load_yaml(input_yaml)
+
+            for key in keyname:
+                json_data = traverse_and_modify(json_data, key, process_string)
+            write_yaml(input_yaml + DECRYPT_SUFF, json_data)
+
+    if file:
+        if encrypt:
+            if arguments['--public-key']:
+                PUBLIC_KEY = arguments['--public-key']
+            check_file(PUBLIC_KEY)
+            for file in input_file:
+                if not file.endswith(DECRYPT_SUFF):
+                    print("!!! WARNING !!!\nAll input files have to have a suffix: '%s' to be able filter them with gitignore etc.\n!!! WARNING !!!" % DECRYPT_SUFF)
+                    sys.exit(1)
+                #encrypt_file(file)
+                file_data = load_file(file)
+                modified = encrypt_RSA_files(file[:-len(DECRYPT_SUFF)], file_data)
+
+
+        if decrypt:
+            if arguments['--private-key']:
+                PRIVATE_KEY = arguments['--private-key']
+            check_file(PRIVATE_KEY)
+            for file in input_file:
+                #decrypt_file(file)
+                modified = decrypt_RSA_files(file)
+                write_file(file + DECRYPT_SUFF, modified)
 
     if listkeys:
         list_keys()
